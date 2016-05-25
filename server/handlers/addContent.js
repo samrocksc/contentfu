7ffa59abf6c397ab76ae65198ae9cbc4abde3aa4
Sam C <samrocksc@gmail.com>
2016-05-25 15:58:46 -0700

    updated hapi routes with fresher handlers

--- server/handlers/addContent.js
+++ server/handlers/addContent.js
@@ -1,3 +1,17 @@
-export default function () {
-  console.log('addRecord');
+module.exports = {
+  index: function(request,reply) {
+    console.log('index');
+  },
+  addContent: function(request,reply) {
+    console.log('addContent');
+  },
+  updateContent: function(request,reply) {
+    console.log('updateContent');
+  },
+  listContent: function(request,reply) {
+    console.log('listContent');
+  },
+  deleteContent: function(request,reply) {
+    console.log('deleteContent');
+  }
 }
--- server/routes/router.js
+++ server/routes/router.js
@@ -1,33 +1,32 @@
-var addRecord = require('../handlers/addRecord');
-
+var handler = require('../handlers/addContent');
 
 module.exports = [
   {
     //creating my index page
     method: 'GET',
     path: '/',
-    handler: addRecord()
+    handler: handler.index
   },
   {
     //just a vanilla get that inserts a record to test database
     method: 'GET',
     path: '/api',
-    handler: addRecord()
+    handler: handler.index
   },
   {
     //creating post data to insert a record endpoint
     method: 'POST',
     path: '/api/',
-    handler: addRecord()
+    handler: handler.index
   },
   {
     method: 'PUT',
     path: '/api/',
-    handler: addRecord()
+    handler: handler.index
   },
   {
     method: 'DELETE',
     path: '/api/{b}',
-    handler: addRecord()
+    handler: handler.index
   }
 ];
