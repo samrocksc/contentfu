a0b23450257f8fd58c92786dc963acf3cae19db4
Sam C <samrocksc@gmail.com>
2016-05-25 15:08:59 -0700

    inserted a jsx server to dev out the frontend

---
+++ app/app.js
@@ -0,0 +1,18 @@
+(function () {
+  let React = require('react/addons');
+  let Main = require('./components/main'); // Our custom react component
+
+  //Needed for React Developer Tools
+  window.React = React;
+
+  //Needed for onTouchTap
+  //Can go away when react 1.0 release
+  //Check this repo:
+  //https://github.com/zilverline/react-tap-event-plugin
+  injectTapEventPlugin();
+
+  // Render the main app react component into the document body.
+  // For more details see: https://facebook.github.io/react/docs/top-level-api.html#react.render
+  React.render(<Main />, document.body);
+
+})();
---
+++ index.jsx
@@ -0,0 +1,13 @@
+require("./node_modules/bootstrap/dist/css/bootstrap.min.css")
+import React from 'react';
+import ReactDOM from 'react-dom';
+
+export class App extends React.Component {
+  render() {
+    return (
+      <div>Simple React + Babel + Bootstrap + Webpack</div>
+    );
+  }
+}
+
+ReactDOM.render(<App/>, document.querySelector("#myApp"));
